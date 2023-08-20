2dbda2d2f51f3f948ed35f7345790f0c15f844a8
Anish Shah <asshah4@users.noreply.github.com>
2023-08-20 15:44:01 -0500

    one step closer for annotations

--- ecg/ludb-segmentation.R
+++ ecg/ludb-segmentation.R
@@ -1,6 +1,7 @@
 # Libraries
 library(fs)
 library(shiva)
+library(ggplot2)
 
 # Data location
 data_loc <- function() {
@@ -24,14 +25,17 @@ dat_1 <- read_wfdb(record = '1', record_dir = ludb)
 hea_1 <- read_header(record = '1', record_dir = ludb)
 ann_i_1 <- read_annotation(record = '1', record_dir = ludb, annotator = 'i')
 
-ecg <- egm(dat_1[, -1], hea_1, ann_i_1)
+ecg <- egm(dat_1, hea_1, ann_i_1)
 
-ggm(ecg, channels = hea1$label)
+ggm(ecg, channels = hea_1$label) |>
+	draw_boundary_mask()
 
 # Labeled data
 # 	Take each P, QRS, T wave region and mask that onto original signal
 # 	This would be similar to "one hot encoding" for each
 # 	Would also needed a "background" or ZERO label
 
-# Input will be 5000 x 12 (10 sec samples @ 500 hertz)
-# Output will be 
+# Input will be 5000 x k (10 sec samples @ 500 hertz; k = number of leads)
+# 	Testing data will be annotated 5000 x 12 x 4 (4 = the encodings)
+# Output will be similar as a seq2seq problem
+# 	5000 x k x 4 (4 = encoding/predictions or labels)
